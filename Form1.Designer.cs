bab61d0fd6aff90980d68b540e014e85a81817d8
NURUL EKA MARYANA <110007326+Nurul-ekamaryana@users.noreply.github.com>
2022-11-21 13:40:25 +0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Tugas_PBO_2_DataBase
+﻿namespace _1from_inset_data
 {
     partial class Form1
     {
@@ -28,131 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtkls = new System.Windows.Forms.TextBox();
+            this.txtalamat = new System.Windows.Forms.TextBox();
+            this.txtjk = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.txtinduk = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtKode = new System.Windows.Forms.TextBox();
-            this.txtNama = new System.Windows.Forms.TextBox();
-            this.btnSimpan = new System.Windows.Forms.Button();
-            this.btnBatal = new System.Windows.Forms.Button();
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.btnHapus = new System.Windows.Forms.Button();
-            this.btnKeluar = new System.Windows.Forms.Button();
-            this.DGTampil = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DGTampil)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // button2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Kode Fakultas";
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(415, 377);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 45);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(415, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 45);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtkls
+            // 
+            this.txtkls.Location = new System.Drawing.Point(157, 143);
+            this.txtkls.Margin = new System.Windows.Forms.Padding(2);
+            this.txtkls.Name = "txtkls";
+            this.txtkls.Size = new System.Drawing.Size(237, 20);
+            this.txtkls.TabIndex = 21;
+            this.txtkls.TextChanged += new System.EventHandler(this.txtkls_TextChanged);
+            // 
+            // txtalamat
+            // 
+            this.txtalamat.Location = new System.Drawing.Point(157, 113);
+            this.txtalamat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtalamat.Name = "txtalamat";
+            this.txtalamat.Size = new System.Drawing.Size(237, 20);
+            this.txtalamat.TabIndex = 20;
+            this.txtalamat.TextChanged += new System.EventHandler(this.txtalamat_TextChanged);
+            // 
+            // txtjk
+            // 
+            this.txtjk.Location = new System.Drawing.Point(157, 80);
+            this.txtjk.Margin = new System.Windows.Forms.Padding(2);
+            this.txtjk.Name = "txtjk";
+            this.txtjk.Size = new System.Drawing.Size(237, 20);
+            this.txtjk.TabIndex = 19;
+            this.txtjk.TextChanged += new System.EventHandler(this.txtjk_TextChanged);
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(157, 48);
+            this.txtnama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(237, 20);
+            this.txtnama.TabIndex = 18;
+            this.txtnama.TextChanged += new System.EventHandler(this.txtnama_TextChanged);
+            // 
+            // txtinduk
+            // 
+            this.txtinduk.Location = new System.Drawing.Point(157, 22);
+            this.txtinduk.Margin = new System.Windows.Forms.Padding(2);
+            this.txtinduk.Name = "txtinduk";
+            this.txtinduk.Size = new System.Drawing.Size(237, 20);
+            this.txtinduk.TabIndex = 17;
+            this.txtinduk.TextChanged += new System.EventHandler(this.txtinduk_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(10, 143);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 25);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Kelas";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(10, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Alamat";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(10, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Jenis Kelamin";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(10, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nama Fakultas";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Nama";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // txtKode
+            // label1
             // 
-            this.txtKode.Location = new System.Drawing.Point(100, 11);
-            this.txtKode.Name = "txtKode";
-            this.txtKode.Size = new System.Drawing.Size(149, 20);
-            this.txtKode.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nomor Induk";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // txtNama
+            // button4
             // 
-            this.txtNama.Location = new System.Drawing.Point(100, 54);
-            this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(311, 20);
-            this.txtNama.TabIndex = 1;
-            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(415, 60);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 45);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // btnSimpan
+            // button5
             // 
-            this.btnSimpan.Location = new System.Drawing.Point(12, 91);
-            this.btnSimpan.Name = "btnSimpan";
-            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
-            this.btnSimpan.TabIndex = 2;
-            this.btnSimpan.Text = "&Simpan";
-            this.btnSimpan.UseVisualStyleBackColor = true;
-            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(415, 109);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 45);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // btnBatal
+            // dataGridView1
             // 
-            this.btnBatal.Location = new System.Drawing.Point(93, 91);
-            this.btnBatal.Name = "btnBatal";
-            this.btnBatal.Size = new System.Drawing.Size(75, 23);
-            this.btnBatal.TabIndex = 3;
-            this.btnBatal.Text = "&Batal";
-            this.btnBatal.UseVisualStyleBackColor = true;
-            this.btnBatal.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.Location = new System.Drawing.Point(174, 91);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 4;
-            this.btnUpdate.Text = "&Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
-            this.btnUpdate.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // btnHapus
-            // 
-            this.btnHapus.Location = new System.Drawing.Point(255, 91);
-            this.btnHapus.Name = "btnHapus";
-            this.btnHapus.Size = new System.Drawing.Size(75, 23);
-            this.btnHapus.TabIndex = 5;
-            this.btnHapus.Text = "&Hapus";
-            this.btnHapus.UseVisualStyleBackColor = true;
-            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
-            // 
-            // btnKeluar
-            // 
-            this.btnKeluar.Location = new System.Drawing.Point(336, 91);
-            this.btnKeluar.Name = "btnKeluar";
-            this.btnKeluar.Size = new System.Drawing.Size(75, 23);
-            this.btnKeluar.TabIndex = 6;
-            this.btnKeluar.Text = "&Keluar";
-            this.btnKeluar.UseVisualStyleBackColor = true;
-            this.btnKeluar.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // DGTampil
-            // 
-            this.DGTampil.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGTampil.Location = new System.Drawing.Point(12, 131);
-            this.DGTampil.Name = "DGTampil";
-            this.DGTampil.Size = new System.Drawing.Size(399, 141);
-            this.DGTampil.TabIndex = 7;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(547, 171);
+            this.dataGridView1.TabIndex = 27;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 286);
-            this.Controls.Add(this.DGTampil);
-            this.Controls.Add(this.btnKeluar);
-            this.Controls.Add(this.btnHapus);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnBatal);
-            this.Controls.Add(this.btnSimpan);
-            this.Controls.Add(this.txtNama);
-            this.Controls.Add(this.txtKode);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(572, 433);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtkls);
+            this.Controls.Add(this.txtalamat);
+            this.Controls.Add(this.txtjk);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.txtinduk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.DGTampil)).EndInit();
+            this.Text = "insert data";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,16 +244,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtkls;
+        private System.Windows.Forms.TextBox txtalamat;
+        private System.Windows.Forms.TextBox txtjk;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.TextBox txtinduk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtKode;
-        private System.Windows.Forms.TextBox txtNama;
-        private System.Windows.Forms.Button btnSimpan;
-        private System.Windows.Forms.Button btnBatal;
-        private System.Windows.Forms.Button btnUpdate;
-        private System.Windows.Forms.Button btnHapus;
-        private System.Windows.Forms.Button btnKeluar;
-        private System.Windows.Forms.DataGridView DGTampil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
